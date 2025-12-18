03a426b09c45bd3fe3d429497706584f8df9c2e6
bala-amypo <bala@amypo.in>
2025-12-18 15:29:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,9 +1,57 @@
 package com.example.demo.entity;
+import java.util.Date;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 @Entity
-public class User{
+public class StudentEntity{
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique=true)
+    private String email;
+    @Column(unique=true)
+    private String rollNumber;
+    public void setId(Integer id){
+        this.id=id;
+    }
+    public void setname(String name){
+        this.name=name;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+     public void setPassword(String password){
+        this.password=password;
+    }
+    public void setCreatedAt(Date createdAt){
+        this.createdAt=createdAt;
+    }
+    public Integer getId(){
+        return id;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
+   
+    public Date getCreatedAt(){
+        return createdAt;
+    }
+    public StudentEntity(Integer id,String username,String email,String password,Date createdAt){
+        this.username=username;
+         this.email=email;
+       this.password=password;
+        this.createdAt=createdAt;
+    }
+    public StudentEntity(){
+
+    }
 }
