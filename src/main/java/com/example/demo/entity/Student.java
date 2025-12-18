0b31b9a26ed95d7e9bab85ff12efb3fc66b71153
bala-amypo <bala@amypo.in>
2025-12-18 15:38:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,9 +1,51 @@
 package com.example.demo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 @Entity
 public class Student{
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Column(
+    private String templateName;
+    @Column(unique=true)
+    private String email;
+    @Column(unique=true)
+    private String rollNumber;
+    public void setId(Long id){
+        this.id=id;
+    }
+    public void setname(String name){
+        this.name=name;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+     public void setRollNumber(String rollNumber){
+        this.rollNumber=rollNumber;
+    }
+    public Long getId(){
+        return id;
+    }
+    public String getname(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getRollNumber(){
+        return rollNumber;
+    }
+    public User(Long id,String name,String email,String rollNumber){
+        this.id=id;
+        this.username=username;
+         this.email=email;
+       this.rollNumber=rollNumber;
+    }
+    public User(){
+
+    }
 }
