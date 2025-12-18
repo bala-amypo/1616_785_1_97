725678062c05bc1768f1114fb7e65265bb23de1f
bala-amypo <bala@amypo.in>
2025-12-18 03:46:09 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/VerificationServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/VerificationServiceImpl.java
@@ -3,7 +3,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.service.VerificationService;
 import com.example.demo.repository.VerificationLogRepository;
-import com.example.demo.entity.Verification;
+import com.example.demo.entity.VerificationLog;
 @Service
 public class VerificationServiceImpl implements VerificationService{
     
