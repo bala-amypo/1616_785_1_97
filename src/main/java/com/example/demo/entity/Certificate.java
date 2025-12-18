f57e087b35892dbeca241abfeb12778426e2e6e0
bala-amypo <bala@amypo.in>
2025-12-18 01:17:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/Certificate.java
+++ src/main/java/com/example/demo/entity/Certificate.java
@@ -1,4 +1,8 @@
 package com.example.demo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 public class Certificate{
     
 }
