3c0c5c04ec9f58e3fe9b63ee907786359c28f443
bala-amypo <bala@amypo.in>
2025-12-18 03:42:14 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/TemplateServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/TemplateServiceImpl.java
@@ -1,4 +1,10 @@
 package com.example.dom.service.Impl;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.service.TemplateService;
+import com.example.demo.repository.TemplateRepository;
+import com.example.demo.entity.Template;
+@Service
 public class TemplateServiceImpl implements TemplateService{
     
 }
