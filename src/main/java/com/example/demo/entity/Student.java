71b8675dabb53b8548d1bb87e6874c03816b4716
bala-amypo <bala@amypo.in>
2025-12-18 16:04:46 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,6 +1,10 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Student {
@@ -17,36 +21,7 @@ public class Student {
     @Column(unique = true, nullable = false)
     private String rollNumber;
 
-    public Student() {}
-
-    public Long getId() { 
-        return id; 
-    }
-    public void setId(Long id) {
-         this.id = id;
-    }
-
-    public String getName() { 
-        return name; 
-    }
-    public void setName(String name) { 
-        this.name = name; 
-    }
-
-    public String getEmail() { 
-        return email; 
-    }
-    public void setEmail(String email) { 
-        this.email = email; 
-    }
-    public String getRollNumber() { 
-        return rollNumber; 
-    }
-    public void setRollNumber(String rollNumber) { 
-        this.rollNumber = rollNumber; 
-    }
     public Student() {
-
     }
 
     public Student(String name, String email, String rollNumber) {
@@ -55,4 +30,35 @@ public class Student {
         this.rollNumber = rollNumber;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
 }
