b0995d56428078d83e3e381d6149b653084f9d88
bala-amypo <bala@amypo.in>
2025-12-18 03:41:02 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/CertificateServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/CertificateServiceImpl.java
@@ -3,7 +3,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.service.CertificateService;
 import com.example.demo.repository.CertificateRepository;
-import com.example.demo.service.CertificateService;
 import com.example.demo.entity.Certificate;
 @Service
 public class CertificateServiceImpl implements CertificateService{
