464026d7fd96296f3e78b263e5451f5e027cb3a6
bala-amypo <bala@amypo.in>
2025-12-20 13:59:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/CertificateTemplate.java
+++ src/main/java/com/example/demo/entity/CertificateTemplate.java
@@ -1,16 +1,12 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@Builder
 public class CertificateTemplate {
 
     @Id
@@ -24,10 +20,57 @@ public class CertificateTemplate {
     private String fontStyle;
     private String signatureName;
 
-    public CertificateTemplate(String templateName, String backgroundUrl, String fontStyle, String signatureName) {
+    public CertificateTemplate() {
+    }
+
+    public CertificateTemplate(
+            String templateName,
+            String backgroundUrl,
+            String fontStyle,
+            String signatureName) {
         this.templateName = templateName;
         this.backgroundUrl = backgroundUrl;
         this.fontStyle = fontStyle;
+        this.signatureName = signatureName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+
+    public String getBackgroundUrl() {
+        return backgroundUrl;
+    }
+
+    public void setBackgroundUrl(String backgroundUrl) {
+        this.backgroundUrl = backgroundUrl;
+    }
+
+    public String getFontStyle() {
+        return fontStyle;
+    }
+
+    public void setFontStyle(String fontStyle) {
+        this.fontStyle = fontStyle;
+    }
+
+    public String getSignatureName() {
+        return signatureName;
+    }
+
+    public void setSignatureName(String signatureName) {
         this.signatureName = signatureName;
     }
 }
