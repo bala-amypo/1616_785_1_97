ffbd1a4540f2de8858f6c6660353b9dfc5a80dee
bala-amypo <bala@amypo.in>
2025-12-26 18:18:20 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,7 +1,5 @@
 package com.example.demo.service;
-
 import com.example.demo.entity.Student;
-
 import java.util.List;
 
 public interface StudentService {
--- src/main/java/com/example/demo/service/VerificationService.java
+++ src/main/java/com/example/demo/service/VerificationService.java
@@ -1,7 +1,5 @@
 package com.example.demo.service;
-
 import com.example.demo.entity.VerificationLog;
-
 import java.util.List;
 
 public interface VerificationService {
